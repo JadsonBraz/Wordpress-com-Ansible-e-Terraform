794ff44433910be58e86194a89a2524f51a48e69
Jadson <jadson2011181@hotmail.com>
2022-02-20 07:56:08 -0300

    feat: add new instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,17 +14,19 @@ provider "aws" {
 
 # Definir as instancias
 resource "aws_instance" "dev" {
-  # count         = 2              # criar 2 instancias
+  count         = 2              # criar 2 instancias
   ami           = var.ami_ubuntu_18["us-east-1"]
   instance_type = "t2.micro"
   key_name      = var.key_name # chave de acesso para ssh
   tags = {
-    Name = "dev-srve-1" # ["dev-srve-${count.index}"] -> dev-srve-1, dev-srve-2, ...
+    Name = "dev-srve-${count.index}" # ["dev-srve-${count.index}"] -> dev-srve-1, dev-srve-2, ...
   }
+
+  security_groups = ["default"]
+
   vpc_security_group_ids = [
     "${aws_security_group.acesso-ssh.id}",
-    "${aws_security_group.acesso-http.id}",
-    "${aws_security_group.acesso-internet.id}"
+    "${aws_security_group.acesso-http.id}"
   ]
 }
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,6 @@
 output "ip_dev" {
-	value = "${aws_instance.dev.public_ip}"
+  value = [
+    "${aws_instance.dev[0].public_ip}",
+    "${aws_instance.dev[1].public_ip}"
+  ]
 }
